5eb60952057be5b94b50d2532908f4a9b931d66f
Abdelrahman mohamed Ahmed <abd_elrahman_elbakry@hotmail.com>
2013-11-26 22:51:48 +0200

    Adding Categories

---
+++ News/CategoriesViewController.h
@@ -0,0 +1,13 @@
+//
+//  CategoriesViewController.h
+//  News
+//
+//  Created by Abdelrahman Mohamed on 11/26/13.
+//  Copyright (c) 2013 Artgine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CategoriesViewController : UITableViewController
+
+@end
